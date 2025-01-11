493091fbc8eb8b3421ad15166b4793c3f49bcee8
magdy shata <magdyshata35@gmail.com>
2025-01-11 07:37:56 +0200

    commit-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,50 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'project-signal';
+
+
+
+  // إنشاء إشارة بقيمة ابتدائية
+  count = signal(0);
+  constructor() {
+    effect(() => {
+      console.log(`The current count is: ${this.count()}`);
+    });
+
+
+
+
+    effect((onCleanup) => {
+      const user = this.currentUser();
+      const timer = setTimeout(() => {
+        console.log(`1 second ago, the user became ${user}`);
+      }, 1000);
+      onCleanup(() => {
+        clearTimeout(timer);
+      });
+    });
+
+  }
+
+   currentUser() {
+    console.log(`1 second ago, the user became `);
+
+  }
+  
+
+
+
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.count.set(22);
+    this.count.update(value => value + 1);
+    // Signals are getter functions - calling them reads their value.
+    console.log('The count is:نشاء إشارة بقيمة ابتدائية ' + this.count());
+
+
+
+  }
 }
+
